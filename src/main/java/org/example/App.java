640e406ae3779fe95c27967c24e2be61285174de
Azenga Kevin <azenga.kevin7@gmail.com>
2021-01-12 12:23:18 +0300

    Wip, basics of the UI

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,11 @@
 package org.example;
 
-import org.example.data.entities.Member;
-import org.example.data.entities.Membership;
 import org.example.data.repositories.MemberRepository;
-import org.example.data.repositories.MembershipRepository;
 import org.example.ui.Display;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Launcher Application
@@ -27,24 +22,8 @@ public class App {
         EntityManager em = emf.createEntityManager();
 
         MemberRepository memberRepo = new MemberRepository(em);
-        MembershipRepository membershipRepo = new MembershipRepository(em);
 
-        Optional<Membership> maybeMembership = membershipRepo.save(new Membership("Swim", 20.00));
-
-        maybeMembership.ifPresent(membership -> {
-
-            Member member = new Member("Dottie", "dottie@gmail.com", "+254700016349", 2, 1174665696L, membership, null);
-            memberRepo.save(member);
-
-            Member anotherMember = new Member("Winny", "winny@gmail.com", "+25498952540", 3, 20931525L, membership, null);
-            memberRepo.save(anotherMember);
-
-            List<Member> members = memberRepo.findAll();
-
-            System.out.println("Members:");
-            members.forEach(System.out::println);
-
-        });
+        memberRepo.findAll().forEach(System.out::println);
 
         em.close();
 
--- src/main/java/org/example/ui/Display.java
+++ src/main/java/org/example/ui/Display.java
@@ -1,6 +1,8 @@
 package org.example.ui;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class Display {
 
@@ -9,6 +11,9 @@ public class Display {
     private int width;
     private int height;
 
+    private JMenuBar theMenuBar;
+    private JPanel theMailPanel;
+
     public Display(String title, int width, int height) {
 
         this.title = title;
@@ -16,6 +21,7 @@ public class Display {
         this.height = height;
 
         initFrame();
+
     }
 
     private void initFrame() {
@@ -23,7 +29,68 @@ public class Display {
         frame = new JFrame(title);
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        initComponents();
+
         frame.setLocationRelativeTo(null);
+
+    }
+
+    private void initComponents() {
+
+        initMenuBar();
+
+        initMainPanel();
+
+    }
+
+    private void initMainPanel() {
+
+        frame.setLayout(new BorderLayout());
+
+        theMailPanel = new JPanel();
+        theMailPanel.setBorder(new EmptyBorder(40, 40, 40, 40));
+        theMailPanel.setLayout(new BorderLayout());
+
+        Box vBox = Box.createVerticalBox();
+
+        JButton registrationModuleButton = new JButton("Registration Module");
+
+        JButton bookingModuleButton = new JButton("Booking Module");
+
+        JButton paymentsModuleButton = new JButton("Payment Module");
+
+        vBox.add(registrationModuleButton);
+        registrationModuleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        vBox.add(Box.createVerticalStrut(20));
+
+        vBox.add(bookingModuleButton);
+        bookingModuleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        vBox.add(Box.createVerticalStrut(20));
+
+        vBox.add(paymentsModuleButton);
+        paymentsModuleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        vBox.add(Box.createVerticalStrut(20));
+
+        theMailPanel.add(vBox, BorderLayout.CENTER);
+
+        frame.add(theMailPanel, BorderLayout.CENTER);
+
+    }
+
+    private void initMenuBar() {
+
+        theMenuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(item -> System.exit(0));
+        fileMenu.add(exitMenuItem);
+        fileMenu.add(fileMenu);
+
+        theMenuBar.add(fileMenu);
+
+        frame.setJMenuBar(theMenuBar);
     }
 
     public void show() {
